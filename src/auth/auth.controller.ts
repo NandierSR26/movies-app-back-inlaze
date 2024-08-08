173dca1d07ac923bceeb4fe9b6ccddcff429e129
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-08 14:48:50 -0500

    feat(auth): Creating a user entity

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,35 @@
-export class Auth {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class Users {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'text',
+  })
+  name: string;
+
+  @Column({
+    type: 'text',
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  avatar?: string;
+
+  @Column({
+    type: 'text',
+  })
+  guest_session_id: string;
+
+  @Column({
+    type: 'boolean',
+    default: false,
+  })
+  active: boolean;
+}
